972b870c6d3073c5c6a0bdc3040ca7c421b512bf
anelya07 <253652@astanait.edu.kz>
2026-03-27 17:52:34 +0500

    A2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
-#TASK 1 - A
+#TASK 2 - A
 
-# a)
 customer = input("Enter customer name: ")
 check = True
 count = 0
@@ -16,31 +15,35 @@ while check == True:
         count = count + 1
         subtotal += price
 
-
-# (b) and (c)
-
 print("=" * 30)
 print("         SHOP RECEIPT")
 print("=" * 30)
 
 print(f"Customer: {customer.upper()}")
 print(f"Items: {count}")
-print(f"Price: {price}" + " KZT")
+print(f"Subtotal: {subtotal}" + " KZT")
 
 print("-" * 30)
 
-if subtotal > 5000:
-    print(f"Subtotal: {subtotal}" + " KZT")
-    discount = subtotal * 0.1
+if subtotal < 3000:
+    discount = 0.0
+    total = subtotal
+    print("Discount tier: No discount")
+    print(f"Discount: {discount}" + " KZT")
+    print(f"Total: {subtotal}" + " KZT")
+
+elif subtotal >= 3000 and subtotal < 7000:
+    discount = subtotal * 0.05
     total = subtotal - discount
     print(f"Discount: {discount}" + " KZT")
     print(f"Total: {total}" + " KZT")
 else:
-    discount = 0
-    total = subtotal
-    print(f"Total: {subtotal}" + " KZT")
+    discount = subtotal * 0.15
+    total = subtotal - discount
+    print(f"Discount: {discount}" + " KZT")
+    print(f"Total: {total}" + " KZT")
 
-# d)
+
 print("-" * 30)
 print("Discount applied: ", subtotal > 5000)
 print("Paid more than 3000: ", total > 3000)
